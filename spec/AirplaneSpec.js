1b03b568290e1fe4b8b4703d532168dbd64df818
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-29 16:06:11 +0100

    implement airplane

---
+++ spec/AirplaneSpec.js
@@ -0,0 +1,21 @@
+describe('Airplane', function() {
+
+  var testAirplane;
+
+  beforeEach(function() {
+    testAirplane = new Airplane();
+  });
+
+  it('can be told to land', function() {
+    expect(testAirplane.land).toBeDefined();
+  });
+
+  it('can be told to takeoff', function(){
+    expect(testAirplane.takeOff).toBeDefined();
+  });
+
+  it('has a isFlying attribute', function(){
+    expect(testAirplane.isFlying).toBeDefined();
+  });
+
+});
---
+++ src/Airplane.js
@@ -0,0 +1,5 @@
+function Airplane(){}
+
+Airplane.prototype.land = function(){};
+Airplane.prototype.takeOff = function(){};
+Airplane.prototype.isFlying = function(){};
